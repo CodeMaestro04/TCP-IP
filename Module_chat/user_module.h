e3fd2484e6e44b57ab02e7920b7302e30d5f39b2
CodeMaestro04 <parkgkgk5096@naver.com>
2024-11-17 14:51:57 +0900

    Update and rename user_module.h to Module_chat/user_module.h

--- Module_chat/user_module.h
+++ Module_chat/user_module.h
@@ -5,13 +5,13 @@
 #define MAX_PW_LEN 21
 #define LEAST_PW_LEN 8
 
-// User ±¸Á¶Ã¼ Á¤ÀÇ
+// User Â±Â¸ÃÂ¶ÃƒÂ¼ ÃÂ¤Ã€Ã‡
 typedef struct {
     char id[MAX_ID_LEN];
     char pw[MAX_PW_LEN];
 } User;
 
-// ÇÔ¼ö ¼±¾ğ
+// Ã‡Ã”Â¼Ã¶ Â¼Â±Â¾Ã°
 int is_valid_id(const char* id);
 int is_valid_pw(const char* pw);
 int find_user_in_log(const char* filename, const char* id, char* pw);
